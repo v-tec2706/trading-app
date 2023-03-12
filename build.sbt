90437566055d4c520b2b05cbb5792e9722fa3cf5
witek <witus2706@gmail.com>
2023-03-12 10:04:33 +0100

    add initial setup

---
+++ build.sbt
@@ -0,0 +1,17 @@
+import Dependencies._
+
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.2.2"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "my-trading-app"
+  )
+
+libraryDependencies ++= Seq(
+  Libraries.cats,
+  Libraries.circeCore,
+  Libraries.circeParser,
+  Libraries.circeRefined
+)
---
+++ project/Dependencies.scala
@@ -0,0 +1,107 @@
+import sbt._
+
+object Dependencies {
+
+  object V {
+    val cats = "2.9.0"
+    val catsEffect = "3.4.8"
+    val circe = "0.14.4"
+    val ciris = "3.0.0"
+    val doobie = "1.0.0-RC2"
+    val flyway = "8.5.13"
+    val fs2Core = "3.6.1"
+    val fs2Kafka = "3.0.0-M9"
+    val http4s = "1.0.0-M39"
+    val http4sMetrics = "1.0.0-M38"
+    val http4sWs = "1.0.0-M3"
+    val ip4s = "3.2.0"
+    val iron = "2.0.0"
+    val kittens = "3.0.0"
+    val monocle = "3.2.0"
+    val natchez = "0.3.1"
+    val natchezHttp4s = "0.5.0"
+    val neutron = "0.7.1"
+    val odin = "0.13.0"
+    val redis4cats = "1.4.0"
+    val refined = "0.10.1"
+
+    val scalajsTime = "2.4.0"
+    val tyrian = "0.6.1"
+
+    val scalacheck = "1.17.0"
+    val weaver = "0.8.1"
+
+    val organizeImports = "0.6.0"
+    val zerowaste = "0.2.5"
+  }
+
+  object Libraries {
+    val cats = "org.typelevel" %% "cats-core" % V.cats
+    val catsEffect = "org.typelevel" %% "cats-effect" % V.catsEffect
+    val fs2Core = "co.fs2" %% "fs2-core" % V.fs2Core
+    val kittens = "org.typelevel" %% "kittens" % V.kittens
+
+    val cirisCore = "is.cir" %% "ciris" % V.ciris
+    val cirisRefined = "is.cir" %% "ciris-refined" % V.ciris
+
+    val circeCore = "io.circe" %% s"circe-core" % V.circe
+    val circeParser = "io.circe" %% s"circe-parser" % V.circe
+    val circeRefined = "io.circe" %% s"circe-refined" % V.circe
+
+    val doobieH2 = "org.tpolecat" %% "doobie-h2" % V.doobie
+    val doobiePg = "org.tpolecat" %% "doobie-postgres" % V.doobie
+    val flyway = "org.flywaydb" % "flyway-core" % V.flyway
+
+    val http4sDsl = "org.http4s" %% s"http4s-dsl" % V.http4s
+    val http4sServer = "org.http4s" %% s"http4s-ember-server" % V.http4s
+    val http4sClient = "org.http4s" %% s"http4s-ember-client" % V.http4s
+    val http4sCirce = "org.http4s" %% s"http4s-circe" % V.http4s
+
+    val http4sJdkWs = "org.http4s" %% "http4s-jdk-http-client" % V.http4sWs
+    val http4sMetrics = "org.http4s" %% "http4s-prometheus-metrics" % V.http4sMetrics
+
+    val ironCore = "io.github.iltotore" %% "iron" % V.iron
+    val ironCats = "io.github.iltotore" %% "iron-cats" % V.iron
+    val ironCirce = "io.github.iltotore" %% "iron-circe" % V.iron
+
+    val ip4sCore = "com.comcast" %% "ip4s-core" % V.ip4s
+
+    val natchezCore = "org.tpolecat" %% "natchez-core" % V.natchez
+    val natchezHoneycomb = "org.tpolecat" %% "natchez-honeycomb" % V.natchez
+    val natchezHttp4s = "org.tpolecat" %% "natchez-http4s" % V.natchezHttp4s
+
+    val neutronCore = "dev.profunktor" %% "neutron-core" % V.neutron
+    val redis4catsEffects = "dev.profunktor" %% "redis4cats-effects" % V.redis4cats
+
+    val monocleCore = "dev.optics" %% "monocle-core" % V.monocle
+    val odin = "com.github.valskalla" %% "odin-core" % V.odin
+
+//    // webapp
+//    val scalajsTime = Def.setting("io.github.cquiroz" %% "scala-java-time" % V.scalajsTime)
+//    val tyrian = Def.setting("io.indigoengine" %% "tyrian" % V.tyrian)
+//    val tyrianIO = Def.setting("io.indigoengine" %% "tyrian-io" % V.tyrian)
+
+    // test
+    val catsLaws = "org.typelevel" %% "cats-laws" % V.cats
+    val monocleLaw = "dev.optics" %% "monocle-law" % V.monocle
+    val scalacheck = "org.scalacheck" %% "scalacheck" % V.scalacheck
+    val weaverCats = "com.disneystreaming" %% "weaver-cats" % V.weaver
+    val weaverDiscipline = "com.disneystreaming" %% "weaver-discipline" % V.weaver
+    val weaverScalaCheck = "com.disneystreaming" %% "weaver-scalacheck" % V.weaver
+
+    // only for demo
+    val fs2Kafka = "com.github.fd4s" %% "fs2-kafka" % V.fs2Kafka
+    val refinedCore = "eu.timepit" %% "refined" % V.refined
+    val refinedCats = "eu.timepit" %% "refined-cats" % V.refined
+
+    // scalafix rules
+    val organizeImports = "com.github.liancheng" %% "organize-imports" % V.organizeImports
+  }
+
+  object CompilerPlugins {
+    val zerowaste = compilerPlugin(
+      "com.github.ghik" % "zerowaste" % V.zerowaste cross CrossVersion.full
+    )
+  }
+
+}
